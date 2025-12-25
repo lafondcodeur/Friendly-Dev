c287b3fdc0b5852b63f944156fb1350ee1e90ec7
lafond <macklafonddeboumas@gmail.com>
2025-12-25 13:28:56 +0100

    Ajout du plugin devtools-json pour React Router v7

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -8,5 +8,6 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  console.log("home");
   return <>My App</>;
 }
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,7 @@
 const ProjectsPage = () => {
   return (
     <section>
-      <h1 className="text-3xl font-bold text-white mb-8 ">Projects Page</h1>
+      <h1 className="text-3xl font-bold text-black mb-8 ">Projects Page</h1>
     </section>
   );
 };
