0c859c0165090e0649fc4c803836aa37691ca8e0
lafond <macklafonddeboumas@gmail.com>
2025-12-26 17:30:42 +0100

    add responsive mobile navigation menu

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router";
-import { FaLaptopCode } from "react-icons/fa";
+import { FaBars, FaLaptopCode, FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const base = "transition hover:text-blue-400";
   const active = "text-blue-400 font-semibold";
   return (
@@ -60,7 +63,68 @@ const NavBar = () => {
             </NavLink>
           </div>
         </div>
+        <div className="md:hidden flex items-center gap-4">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-blue-400 text-xl cursor-pointer"
+            title="Menu"
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
       </div>
+      {/* Mobile Nav */}
+      {menuOpen && (
+        <div className=" md:hidden bg-gray-800 border-t border-gray-700 px-6 py-4 space-y-2 space-x-4 text-center">
+          <div className="space-x-4 text-sm text-gray-300 ">
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? "opacity-50" : isActive ? active : base
+              }
+              onClick={() => setMenuOpen(false)}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? "opacity-50" : isActive ? active : base
+              }
+              onClick={() => setMenuOpen(false)}
+              to="/about"
+            >
+              About
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? "opacity-50" : isActive ? active : base
+              }
+              onClick={() => setMenuOpen(false)}
+              to="/projects"
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? "opacity-50" : isActive ? active : base
+              }
+              onClick={() => setMenuOpen(false)}
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? "opacity-80" : isActive ? active : base
+              }
+              onClick={() => setMenuOpen(false)}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
