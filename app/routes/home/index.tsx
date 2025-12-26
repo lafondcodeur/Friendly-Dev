3e4c46e395952bcd78c89cb4bd87e4616a209c68
lafond <macklafonddeboumas@gmail.com>
2025-12-26 18:58:23 +0100

    add hero section to homepage

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,3 +1,4 @@
+import Hero from "~/component/hero";
 import type { Route } from "./+types/index";
 
 export function meta({}: Route.MetaArgs) {
@@ -9,5 +10,9 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   console.log("home");
-  return <>My App</>;
+  return (
+    <section>
+      <Hero name="MACK LAFOND" />
+    </section>
+  );
 }
