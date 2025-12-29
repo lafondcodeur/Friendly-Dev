98af4293dfb5d5161e0667118c3a087837cbd010
lafond <macklafonddeboumas@gmail.com>
2025-12-29 19:02:32 +0100

    Typage du modèle Project et des données chargées via loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,14 +1,17 @@
 import { useLoaderData } from "react-router";
 import type { Route } from "./+types";
+import type { Project } from "~/type";
 
-export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch("http://localhost:8000/projects");
   const data = await res.json();
   return { projects: data };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData;
+  const { projects } = loaderData as { projects: Project[] };
   console.log(projects);
   return (
     <>
